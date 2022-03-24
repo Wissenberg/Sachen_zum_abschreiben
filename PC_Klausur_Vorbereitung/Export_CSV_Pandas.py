8ebcbfdc291af6e01a6b346aa543410dd1344596
Wissenberg <74718217+Wissenberg@users.noreply.github.com>
2022-03-24 11:22:29 +0100

    Testat 3 und 4

--- PC_Klausur_Vorbereitung/Export_CSV_Pandas.py
+++ PC_Klausur_Vorbereitung/Export_CSV_Pandas.py
@@ -15,7 +15,6 @@ class DBMapper:
     def toCSV(self):
         sql_query = pd.read_sql_query('SELECT * FROM test;', self.con)
         sql_query.to_csv('test.csv', index = False, line_terminator = '\n',sep=";")
-
 if __name__ == '__main__':
     db = DBMapper()
     db.toCSV()
